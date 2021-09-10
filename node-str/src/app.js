533a597c660ab912f0e566bb55bbbc2060c180cb
sarahkalebe <47755793+sarahkalebe@users.noreply.github.com>
2021-09-10 15:31:01 -0300

    Add files via upload

---
+++ node-str/src/app.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+const router = express.Router();
+
+//Conecta ao Banco
+mongoose.connect('mongodb+srv://aline:alinebanco@cluster0.eyjlk.mongodb.net/test');
+
+//Carrega os Models
+const Product = require('./models/product');
+
+//Carrega as Rotas
+const indexRoute = require('./routes/index-route');
+const productRoute = require('./routes/product-route');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use('/', indexRoute);
+app.use('/products', productRoute);
+
+module.exports = app;
